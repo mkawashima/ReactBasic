9748c5eb9a36534a6f6ad6172596eb33f345466b
mkawashima <mkawashima@users.noreply.github.com>
2021-04-01 19:54:01 +0900

    styleの割当

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert();
-
+  const contentStyle = {
+    color: "blue",
+    fontize: "18px"
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <h1 style={contentStyle}>こんにちは！</h1>
+      <p style={{ color: "red" }}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
